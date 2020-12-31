9b49bb851226a38fa2e603ff27743e223be5ade4
Yifang Di <diyifang@ymail.com>
2020-12-31 10:32:49 -0500

    new year countdown

---
+++ newyear-countdown/script.js
@@ -0,0 +1,32 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+year.innerText = currentYear + 1;
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+}
+setInterval(updateCountdown, 1000);
+//show spinner
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
